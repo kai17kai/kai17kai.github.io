5cc0cb269fcf2d19325a9ff8d0be9084f282e195
kai17kai <75098098+kai17kai@users.noreply.github.com>
2020-12-18 21:08:52 -0800

    Update Game.js

--- Another Game/Game.js
+++ Another Game/Game.js
@@ -15,26 +15,58 @@ clear();
 
 context.fillStyle = "rgb(255, 255, 255)"
 context.fillRect(10, 10, 10, 10);
+var BallPosition = [100, 10];
 context.fillRect(400, 500, 50, 10)
 
-var x = 400;
+var HumanX = 400;
 function Movement(e) {
     if (e.key === "d") {
-        x += 10;
-        if (x > 750) {
-            x -= 10;
+        HumanX += 10;
+        if (HumanX === 800) {
+            HumanX -= 10;
         }
     } else if (e.key === "a") {
-        x -= 10;
-        if (x < 0) {
-            x += 10
+        HumanX -= 10;
+        if (HumanX === 0) {
+            HumanX += 10
         }
     }
 
-    console.log(String(x));
+    console.log(String(HumanX))
+}
+
+function Visibility() {
     clear();
-    context.fillRect(x, 500, 50, 10);
+    context.fillRect(HumanX, 500, 60, 10);
+    context.fillRect(BallPosition[0], BallPosition[1], 10, 10)
+}
+
+var Up, Down = true, Left, Right;
+
+function BallMovement() {
+    if (Up === true) {
+        BallPosition[1] -= 10;
+        if (BallPosition[1] === 0) {
+            Up = false;
+            Down = true;
+        }
+    }
+    if (Down === true) {
+        BallPosition[1] += 10;
+        if (BallPosition[1] === 500 && BallPosition[0] >= HumanX && BallPosition[0] <= HumanX + 50) {
+            Down = false;
+            Up = true;
+        }
+    }
+    if (Left === true) {
+        BallPosition[0] -= 10;
+    }
+    if (Right === true) {
+        BallPosition[0] += 10;
+    }
 }
 
 document.addEventListener("keydown", Movement);
+var BallVisibility = setInterval(Visibility, 1);
+var Ball = setInterval(BallMovement, 100);
 
