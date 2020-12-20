7463ff7891d602406d76fe6d78c4367b4fd713af
kai17kai <75098098+kai17kai@users.noreply.github.com>
2020-12-20 14:15:14 -0800

    gift

---
+++ index.js
@@ -0,0 +1,52 @@
+function Event() {
+
+    //Kyle's Audio
+    var KyleAudio = document.createElement("audio");
+    KyleAudio.src = "Audio/Kyle Audio.mp3";
+    KyleAudio.className = "Audio";
+    KyleAudio.controls = "controls";
+    
+    //Ethan's Audio
+    var EthanAudio = document.createElement("audio");
+    EthanAudio.src = "Audio/Ethan Audio.mp3";
+    EthanAudio.className = "Audio";
+    EthanAudio.controls = "controls";
+
+    //Paragraph on DOM
+    var Paragraph = document.getElementById("Paragraph");
+
+    //Audio
+    var Audio = document.getElementsByClassName("Audio");
+
+    //Radio Buttons
+    var KyleButton = document.getElementById("Kyle");
+    var EthanButton = document.getElementById("Ethan");
+    var Remove = document.getElementById("Remove");
+
+    if (KyleButton.checked === true) {
+        document.body.appendChild(KyleAudio);
+         Paragraph.innerHTML = Words(1);
+    } else if (EthanButton.checked === true) {
+        document.body.appendChild(EthanAudio);
+        Paragraph.innerHTML = Words(2);
+    } else if (Remove.checked === true) {
+        Paragraph.innerHTML = " ";
+        if (Audio != null) {
+            for (let i = 0; i <= Audio.length; ++i) {
+                Audio[i].remove();
+            }
+        }
+    }
+}
+
+function Words(Number) {
+    if (Number === 1) {
+        return "Kyle's Audio"
+    } else if (Number === 2) {
+        return "Ethan's Audio"
+    } else if (Number === 3) {
+        return "Alex's Audio"
+    } else if (Number === 4) {
+        return "Annie's Audio"
+    }
+}
