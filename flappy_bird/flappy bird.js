0ef6521e858ba8ac4507fc6cc76ca3e48f481672
kai17kai <75098098+kai17kai@users.noreply.github.com>
2021-01-02 11:21:48 -0800

    d

--- flappy_bird/flappy bird.js
+++ flappy_bird/flappy bird.js
@@ -8,17 +8,36 @@ context.fillStyle = "rgb(255, 255, 255)";
 
 function clear() {
     let temp = context.fillStyle;
-    context.fillStyle = "rgb(0, 0, 0)";
+    context.fillStyle = "rgb(255, 255, 255)";
     context.fillRect(0, 0, canvas.width, canvas.height);
     context.fillStyle = temp;
 }
 clear();
 
-//Bird Position
-var BirdPosition = {
-    x: 
-}
+var ObstaclesPositions = new Array;
 
-function Jump() {
+var CreateObstacles = setInterval(() => {    
+    ObstaclesPositions.push(730);
+    ObstaclesPositions.push(Math.floor(Math.random() * 450 - 300 + 1) + 100);
+}, 1500);
 
-}
+context.fillStyle = "rgb(0, 0, 0)";
+
+var MoveObstacles = setInterval(() => {
+    for (let x = 0; x < ObstaclesPositions.length; x += 2) {
+        if (ObstaclesPositions[x] + 50 >= 0) { 
+            ObstaclesPositions[x] -= 1
+        } else {
+            for (let x = 1; x <= 2; ++x) {
+                ObstaclesPositions.shift();
+            }
+        }
+    }
+}, 10)
+
+var Visibility = setInterval(() => {
+    clear()
+    for (let x = 0; x < ObstaclesPositions.length; x += 2) {
+        context.fillRect(ObstaclesPositions[x], 0, 50, ObstaclesPositions[x + 1]);
+    }
+})
