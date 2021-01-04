ae4d55e53f51db884efe3a12ccbede584b7c2946
kai17kai <75098098+kai17kai@users.noreply.github.com>
2021-01-03 21:37:40 -0800

    A LOT MORE FUCKS

--- flappy_bird/flappy bird.js
+++ flappy_bird/flappy bird.js
@@ -9,24 +9,18 @@ var ObstaclesPositions = new Array;
 //Holds player's y location
 var Player = 280;
 
-function Reload() {
-    while (ObstaclesPositions.length > 0) {
-        ObstaclesPositions.pop();
-    }
-    context.clearRect(0, 0, canvas.width, canvas.height);
-}
-
-var CreateObstacles = setInterval(() => {    
-    ObstaclesPositions.push(800);
-    ObstaclesPositions.push(Math.floor(Math.random() * 450 - 400 + 1) + 400);
-}, 2500);
-
 //Moves the obstacles
 function MoveObstacles() {
     for (let x = 0; x < ObstaclesPositions.length; x += 2) {
         ObstaclesPositions[x] -= 4;
     }
 }
+
+//creates obstacles at 2.5 seconds
+var CreateObstacles = setInterval(() => {    
+    ObstaclesPositions.push(800);
+    ObstaclesPositions.push(Math.floor(Math.random() * 450 - 400 + 1) + 400);
+}, 2500);
 
 //allows player to see themselves and the obstacles
 var Visibility = setInterval(() => {
@@ -37,12 +31,13 @@ var Visibility = setInterval(() => {
         if (ObstaclesPositions[x] >= 0) {
             context.fillRect(ObstaclesPositions[x] + 4, 0, 50, ObstaclesPositions[x + 1]);
             context.fillRect(ObstaclesPositions[x] + 4, ObstaclesPositions[x + 1] + 100, 50, canvas.height);
-        }
-    }
-
-    if (ObstaclesPositions[0] < 0 && ObstaclesPositions[1] < 0) {
-        for (let x = 0; x < 2; ++x) {
-            ObstaclesPositions.shift();
+            if (ObstaclesPositions[x] < 0) {
+                ObstaclesPositions[x] = null;
+                ObstaclesPositions[x + 1] = null;
+                for (let i = 0; i < 1; ++i) {
+                    ObstaclesPositions.shift();
+                }
+            }
         }
     }
 
@@ -63,4 +58,7 @@ var Visibility = setInterval(() => {
 
     context.arc(100, Player, 20, 0, 2 * Math.PI, false);
     context.fill();
-}, 40);
+
+    temp = null;
+    delete temp;
+}, 30);
