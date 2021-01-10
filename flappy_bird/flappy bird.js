367f172e9b96dd13a52283c208092897fe134a4b
kai17kai <75098098+kai17kai@users.noreply.github.com>
2021-01-10 15:47:49 -0800

    Update flappy bird.js

--- flappy_bird/flappy bird.js
+++ flappy_bird/flappy bird.js
@@ -5,6 +5,20 @@ canvas.height = 600;
 canvas.width = 800;
 const context = canvas.getContext("2d");
 
+context.font = "20px Arial";
+
+/*----------------------------------------------------------------*/
+//Set up time
+var sectime = 0, mintime = 0;
+setInterval(() => {
+    sectime += 1;
+    if (sectime > 59) {
+        sectime = 0;
+        mintime += 1;
+    }
+}, 1000);
+/*----------------------------------------------------------------*/
+
 //Array that holds the positions of the obstacles
 var ObstaclesPositions = new Array;
 
@@ -48,24 +62,19 @@ const Game = () => {
     context.closePath();
     context.fill();
 
+    if (sectime < 9 && mintime < 9) {
+        context.fillText(`0${mintime}:0${sectime}`, 0, 50);
+    } else if (sectime < 9 && mintime > 9) {
+        context.fillText(`${mintime}:0${sectime}`, 0, 50);
+    } else if (sectime > 9 && mintime < 9) {
+        context.fillText(`0${mintime}:${sectime}`, 0, 50);
+    } else if (sectime > 9 && mintime > 9) {
+        context.fillText(`${mintime}:${mintime}`, 0, 50);
+    }
+
+
     if (Player >= canvas.height || Player <= 0) {
-        context.fillStyle = "rgb(255, 255, 255)";
-
-        for (let x = 0; x < ObstaclesPositions.length; x += 2) {
-            if (ObstaclesPositions[x] >= 0) {
-                context.clearRect(ObstaclesPositions[x], 0, 50, ObstaclesPositions[x + 1]);
-                context.clearRect(ObstaclesPositions[x], ObstaclesPositions[x + 1] + 125, 50, canvas.height - ObstaclesPositions[x + 1] + 125);
-                AtEdge(x);
-            }
-        }
-    
-        context.beginPath();
-        context.arc(100, Player - 2, 30, 0, 2 * Math.PI, false);
-        context.closePath();
-        context.fill();
-
-        clearInterval(Visibility);
-        clear();
+        context.clearRect(0, 0, canvas.width, canvas.height);
         context.fillStyle = temp;
         context.font = "40px Arial";
         context.fillText("You Died", 380, 280);
@@ -80,10 +89,11 @@ const Game = () => {
             for (let i = 0; i < 1; ++i) {
                 ObstaclesPositions.shift();
             }
-        }
+        }  
     }
 
     function clear() {
+        context.clearRect(0, 30, 50, 50);
         for (let x = 0; x < ObstaclesPositions.length; x += 2) {
             if (ObstaclesPositions[x] >= 0) {
                 context.clearRect(ObstaclesPositions[x] + 4, 0, 50, ObstaclesPositions[x + 1]);
@@ -96,10 +106,10 @@ const Game = () => {
             context.beginPath();
             context.arc(100, Player - 4, 30, 0, 2 * Math.PI, false);
             context.closePath();
-            context.fill();
+            context.fill(); 
         } else {
             context.beginPath();
-            context.arc(100, Player + 4, 30, 0, 2 * Math.PI, false);
+            context.arc(100, Player + 6, 30, 0, 2 * Math.PI, false);
             context.closePath();
             context.fill();
         }
@@ -114,19 +124,18 @@ var Gravity = setInterval(() => {
     if (Up == false) {
         Player += Velocity;
     } else {
-        Player -= Velocity;
+        Player -= 6;
     }
-}, 30);
+}, 30); 
 
 document.addEventListener("keydown", (e) => {
-    if (Visibility != null) {
+    if (e.key === " ") {
         Up = true;
-        console.log(e.key);
     }
 });
 
 document.addEventListener("keyup", (e) => {
-    if (Visibility != null) {
+    if (e.key === " ") {
         Up = false;
     }
-})
+});
