0262585ca8fcad228782fe27eaddeedaa6380906
kai17kai <75098098+kai17kai@users.noreply.github.com>
2021-01-19 17:32:12 -0800

    Update hangman.js

--- hangman/hangman.js
+++ hangman/hangman.js
@@ -109,16 +109,13 @@ var Game = (e) => {
         document.getElementById("list").innerHTML = l;
 
         if (LinePositions.length == UsePositions.length || WrongGuess.length == ChosenWord.length) {
-            setTimeout(() => {
-                context.clearRect(0, 0, canvas.width, canvas.height);
-                context.fillText("You Have Won The Game. Press y to restart the game", 300, 300);
-                NotOver = false;
-                document.addEventListener("keydown", (e) => {
-                    if (e.key == "y" || e.key == "Y") {
-                        window.location.reload();
-                    }
-                });
-            }, 1000);
+            context.fillText("You Have Won The Game. Press y to restart the game", 300, 400);
+            NotOver = false;
+            document.addEventListener("keydown", (e) => {
+                if (e.key == "y" || e.key == "Y") {
+                    window.location.reload();
+                }
+            });
         }
     }
 }
