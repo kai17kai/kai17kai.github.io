434649c3f27f046097a2782be82de4651352bdb5
kai17kai <75098098+kai17kai@users.noreply.github.com>
2021-02-08 15:19:41 -0800

    fix stuff

--- snake_game/snake_game.js
+++ snake_game/snake_game.js
@@ -10,6 +10,8 @@ snake_x.push(Math.floor(Math.random() * 30) * 20);
 let snake_y = [];
 snake_y.push(Math.floor(Math.random() * 30) * 20);
 
+let score = 0;
+
 let food = {
     x: Math.floor(Math.random() * 40) * 20,
     y: Math.floor(Math.random() * 30) * 20
@@ -34,6 +36,7 @@ window.onkeydown = (e) => {
 }
 
 let game = setInterval(() => {
+    document.getElementById("length").innerHTML = `Score: ${score}`;
     let distance = Math.sqrt(Math.pow(snake_x[0] - food.x, 2) + Math.pow(snake_y[0] - food.y, 2));
     if (distance <= 20) {
         for (let i = 0; i < 2; ++i) {
@@ -53,6 +56,7 @@ let game = setInterval(() => {
     }
         food.x = Math.floor(Math.random() * 40) * 20;
         food.y = Math.floor(Math.random() * 30) * 20;
+        ++score;
     }
     if (up) {
         snake_y.unshift(snake_y[0] - 10);
@@ -97,8 +101,6 @@ let game = setInterval(() => {
 
     context.fillStyle = "rgb(255, 0, 0)";
     context.fillRect(food.x, food.y, 20, 20);
-
-    document.getElementById("length").innerHTML = `Score: ${snake_x.length - 1}`;
 
     for (let i = 1; i < snake_x.length; ++i) {
         let distance = Math.sqrt(Math.pow(snake_x[0] - snake_x[i], 2) + Math.pow(snake_y[0] - snake_y[i], 2)) + 10;
