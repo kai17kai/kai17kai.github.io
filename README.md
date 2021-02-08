5c7681bb2632a0daafb5175a0c3c23f5dc521356
kai17kai <75098098+kai17kai@users.noreply.github.com>
2021-02-07 19:23:51 -0800

    broke game of snake

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # kai17kai.github.io
 
-This is a website that I have created. Hope you enjoy.
+This is a website that I have created. Hope you enjoy!
---
+++ social/snake_game/snake_game.js
@@ -0,0 +1,123 @@
+"use strict";
+
+const canvas = document.getElementById("canvas");
+canvas.height = 600;
+canvas.width = 800;
+const context = canvas.getContext("2d");
+
+let snake_x = [];
+snake_x.push(Math.floor(Math.random() * 30) * 20);
+let snake_y = [];
+snake_y.push(Math.floor(Math.random() * 30) * 20);
+
+let food = {
+    x: Math.floor(Math.random() * 40) * 20,
+    y: Math.floor(Math.random() * 30) * 20
+}
+
+let up = false, down = false, right = false, left = false;
+window.onkeydown = (e) => {
+    if ((e.key === "w" || e.key  === "ArrowUp") && down === false) {
+        up = false, down = false, right = false, left = false;
+        up = true;
+    } else if ((e.key === "s" || e.key === "ArrowDown") && up === false) {
+        up = false, down = false, right = false, left = false;
+        down = true;
+    } else if ((e.key === "d" || e.key === "ArrowRight") && left === false) {
+        up = false, down = false, right = false, left = false;
+        right=true;
+    } else if ((e.key === "a" || e.key === "ArrowLeft") && right === false) {
+        up = false, down = false, right = false, left = false;
+        left=true;
+    }
+}
+
+let game = setInterval(() => {
+    if (snake_x[0] === food.x && snake_y[0] === food.y) {
+        snake_x.unshift(food.x);
+        snake_y.unshift(food.y);
+        food.x = Math.floor(Math.random() * 40) * 20;
+        food.y = Math.floor(Math.random() * 30) * 20;
+    }
+
+    if (up) {
+        /*
+        for (let i = snake_y.length - 1; i > 0; --i) {
+            snake_y[i] = snake_y[i - 1];
+        }
+        snake_y[0] -= 20;
+        */
+        snake_y.unshift(snake_y[0] - 20);
+        snake_x.unshift(snake_x[0]);
+        snake_y.pop();
+        snake_x.pop();
+    } else if (down) {
+        /*
+        for (let i = snake_y.length - 1; i > 0; --i) {
+            snake_y[i] = snake_y[i - 1];
+        }
+        snake_y[0] += 20;
+        */
+        snake_y.unshift(snake_y[0] + 20);
+        snake_x.unshift(snake_x[0]);
+        snake_x.pop();
+        snake_y.pop();
+    } else if (right) {
+        /*
+        for (let i = snake_x.length - 1; i > 0; --i) {
+            snake_x[i] = snake_x[i - 1];
+        }
+        snake_x[0] += 20;
+        */
+        snake_x.unshift(snake_x[0] + 20);
+        snake_y.unshift(snake_y[0]);
+        snake_x.pop();
+        snake_y.pop();
+    } else if (left) {
+        /*
+        for (let i = snake_x.length - 1; i > 0; --i) {
+            snake_x[i] = snake_x[i - 1];
+        }
+        snake_x[0] -= 20;
+        */
+        snake_x.unshift(snake_x[0] - 20);
+        snake_y.unshift(snake_y[0]);
+        snake_x.pop();
+        snake_y.pop();
+    }
+
+    for (let i = 0; i < snake_x.length; ++i) {
+        if (snake_x[i] === -20) {
+            snake_x[i] = 800;
+        } else if (snake_x[i] === 800) {
+            snake_x[i] = 0;
+        } else if (snake_y[i] === -20) {
+            snake_y[i] = 580;
+        } else if (snake_y[i] === 600) {
+            snake_y[i] = 0;
+        }
+    }
+
+    clear();
+
+    context.fillStyle = "rgb(0,255,0)";
+    for (let i = 0; i < snake_x.length; ++i) {
+        context.fillRect(snake_x[i], snake_y[i], 20, 20);
+    }
+
+    context.fillStyle = "rgb(255, 0, 0)";
+    context.fillRect(food.x, food.y, 20, 20);
+    if (snake_x.lastIndexOf(snake_x[0]) && snake_y.lastIndexOf(snake_y[0])) {
+        clear();
+        context.font = "40px Arial";
+        context.fillText("You Have Died", 400, 300);
+        clearInterval(game);
+    }
+}, 100);
+
+function clear() {
+    let temp = context.fillStyle;
+    context.fillStyle = "rgb(0, 0, 0)";
+    context.fillRect(0, 0, canvas.width, canvas.height);
+    context.fillStyle = temp;
+}
