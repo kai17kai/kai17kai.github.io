e4eba3da09641edf703243172ef77667ff397c7c
kai17kai <75098098+kai17kai@users.noreply.github.com>
2021-04-20 16:11:13 -0700

    add some animation

---
+++ mix_that_cup/cup.js
@@ -0,0 +1,25 @@
+let cups = Array.from(document.getElementsByClassName("cup"));
+let answer = Math.floor(Math.random() * 3);
+
+
+for (let i = 0; i < cups.length; ++i) {
+    cups[i].onclick = (e) => {
+        cups[i].parentElement.style.animation = "MoveUp 0.2s forwards"
+        let pTag = document.createElement("p");
+        if (Number(cups[i].id) - 1 == answer) {
+            let pText = document.createTextNode("You have won the game");
+            pTag.appendChild(pText);
+            cups[i].parentElement.insertBefore(pTag, cups[i]);
+            for (let j = 0; j < 3; ++j) {
+                cups[i].onclick = null;
+            }
+        } else {
+            let pText = document.createTextNode("You have lost the game");
+            pTag.appendChild(pText);
+            cups[i].parentElement.insertBefore(pTag, cups[i]);
+            for (let j = 0; j < 3; ++j) {
+                cups[i].onclick = null;
+            }
+        }
+    }
+}
