b85561edd34e713a0f30471b64d272030e441ee6
kai17kai <75098098+kai17kai@users.noreply.github.com>
2021-05-20 13:20:48 -0700

    add maze game

--- maze_game/maze.js
+++ maze_game/maze.js
@@ -13,258 +13,69 @@ let gameLevel = 20;
 let board = [];
 
 //range of colors
-let colors = ["green", "white", "red", "rgb(78, 49, 30)"];
+let colors = ["lime", "lightgray", "red", "rgb(78, 49, 30)"];
 
 range.value = square_side.toString();
 range.onchange = (e) => {
     let side = e.target.value;
     canvas.setAttribute('width', side.toString());
     canvas.setAttribute('height',side.toString());
-    DisplayMap();
+    !died ? DisplayMap() : Player.Died();
 }
 
-function MoveUp(pos, who = 0) {
-    if (who == 0) {
-        var iter = board.indexOf(1);
-        board[iter] = 0;
+function SeeAroundPos(position) {
+    let xOffsets = [-1, 0, 1, 0];
+    let yOffsets = [0, -1, 0, 1];
+    let OpenPositions = [];
+    for (let i = 0; i < 4; ++i) {
+        let x = position % gameLevel;
+        let y = Math.floor(position / gameLevel);
+        x += xOffsets[i];
+        y += yOffsets[i];
+        if (x >= 0 && y >= 0 && x < gameLevel && y < gameLevel && (board[y * gameLevel + x] === 0 || board[y * gameLevel + x] === 3)) {
+            OpenPositions.push(y * gameLevel + x);
+        }
     }
-    else {
-        var iter = board.indexOf(4);
-        board[iter] = 2;
-    }
-    
-    if (pos > 0) {
-        pos -= 1;
-        iter += gameLevel * -1;
-    }
-
-    who == 0 ? board[iter] = 1 : board[iter] = 4;
-    return pos;
-}
-
-function MoveRight(pos, who = 0) {
-    if (who === 0) {
-        var iter = board.indexOf(1);
-        board[iter] = 0;
-    } else {
-        var iter = board.indexOf(4);
-        board[iter] = 2;
-    }
-    if (pos < gameLevel - 1) {
-        pos += 1;
-        iter += 1;
-    }
-    who === 0 ? board[iter] = 1 : board[iter] = 4;
-    return pos;
-}
-
-function MoveDown(pos, who = 0) {
-    if (who === 0) {
-        var iter = board.indexOf(1);
-        board[iter] = 0;
-    } else {
-        var iter = board.indexOf(4);
-        board[iter] = 2;
-    }
-
-    if (pos < gameLevel - 1) {
-        pos += 1;
-        iter += gameLevel;
-    }
-
-    who === 0 ? board[iter] = 1 : board[iter] = 4;
-    return pos;
-}
-
-function MoveLeft(pos, who = 0)
-{
-	if (who == 0) {
-		var iter = board.indexOf(1);
-		board[iter] = 0;
-	}
-	else {
-		var iter = board.indexOf(4);
-		board[iter] = 2;
-	}
-
-
-	if (pos > 0)
-	{
-		pos -= 1;
-		iter -= 1;
-	}
-
-	who == 0 ? board[iter] = 1 : board[iter] = 4;
-	return pos;
+    return OpenPositions;
 }
 
 function CreateMap() {
     board = new Array(gameLevel * gameLevel).fill(0);
-    let ComX = 0, ComY = 0;
     board[0] = 4;
     board[board.length - 1] = 3;
-    let PastMoves = [];
-    while (board.includes(3)) {
-        let Move = Math.floor(Math.random() * 4) + 1;
-        let position = board.indexOf(4);
-        if (Move === 1) {
-            if (ComY > 0) {
-                if (board[position - gameLevel] != 2) {
-                    PastMoves.push('w');
-                    ComY = MoveUp(ComY, 1);
-                }
+    let MissedMoves = [];
+    let PosIsOpen = true;
+    while (PosIsOpen) {
+        let PastPosition = board.indexOf(4);
+        let OpenPositions = SeeAroundPos(PastPosition);
+        if (OpenPositions.length > 0) {
+            let RandomIndex = Math.floor(Math.random() * OpenPositions.length);
+            let RandomPosition = OpenPositions[RandomIndex];
+            OpenPositions.splice(RandomIndex, 1);
+            board[PastPosition] = 2;
+            board[RandomPosition] = 4;
+            if (MissedMoves.includes(RandomPosition)) {
+                MissedMoves.splice(MissedMoves.indexOf(RandomPosition), 1);
             }
-            else {
-                for (let x = 0; x < 2; ++x) {
-                    if (PastMoves.length > 3) {
-                        if (PastMoves[PastMoves.length - 1] === 'w') {
-                            let iter = board.indexOf(4);
-                            board[iter] = 0;
-                            ComY += 1;
-                            iter += gameLevel;
-                            board[iter] = 4;
-                        } else if (PastMoves[PastMoves.length - 1] === "s") {
-                            let iter = board.indexOf(4);
-                            board[iter] = 0;
-                            ComY -= 1;
-                            iter -= gameLevel;
-                            board[iter] = 4;
-                        } else if (PastMoves[PastMoves.length - 1] === "d") {
-                            let iter = board.indexOf(4);
-                            board[iter] = 0;
-                            ComX -= 1;
-                            iter -= 1;
-                            board[iter] = 4;
-                        } else if (PastMoves[PastMoves.length - 1] === "a") {
-                            let iter = board.indexOf(4);
-                            board[iter] = 0;
-                            ComX += 1;
-                            iter += 1;
-                            board[iter] = 4;
-                        }
-                        PastMoves.pop();
-                    }
+            OpenPositions.forEach(element => {
+                if (!MissedMoves.includes(element)) {
+                    MissedMoves.push(element);
                 }
-            }
-        } else if (Move === 2) {
-            if (ComX < gameLevel - 1) {
-                if (board[position + 1] != 2) {
-                    PastMoves.push("d");
-                    ComX = MoveRight(ComX, 1);
-                }
-            }
-            else {
-                for (let x = 0; x < 2; ++x) {
-                    if (PastMoves.length > 3) {
-                        if (PastMoves[PastMoves.length - 1] === 'w') {
-                            let iter = board.indexOf(4);
-                            board[iter] = 0;
-                            ComY += 1;
-                            iter += gameLevel;
-                            board[iter] = 4;
-                        } else if (PastMoves[PastMoves.length - 1] === "s") {
-                            let iter = board.indexOf(4);
-                            board[iter] = 0;
-                            ComY -= 1;
-                            iter -= gameLevel;
-                            board[iter] = 4;
-                        } else if (PastMoves[PastMoves.length - 1] === "d") {
-                            let iter = board.indexOf(4);
-                            board[iter] = 0;
-                            ComX -= 1;
-                            iter -= 1;
-                            board[iter] = 4;
-                        } else if (PastMoves[PastMoves.length - 1] === "a") {
-                            let iter = board.indexOf(4);
-                            board[iter] = 0;
-                            ComX += 1;
-                            iter += 1;
-                            board[iter] = 4;
-                        }
-                        PastMoves.pop();
-                    }
-                }
-            }
-        } else if (Move === 3) {
-            if (ComY < gameLevel - 1) {
-                if (board[position + gameLevel] != 2) {
-                    PastMoves.push("s");
-                    ComY = MoveDown(ComY, 1);
-                }
-                else {
-                    for (let x = 0; x < 2; ++x) {
-                        if (PastMoves.length > 3) {
-                            if (PastMoves[PastMoves.length - 1] === 'w') {
-                                let iter = board.indexOf(4);
-                                board[iter] = 0;
-                                ComY += 1;
-                                iter += gameLevel;
-                                board[iter] = 4;
-                            } else if (PastMoves[PastMoves.length - 1] === "s") {
-                                let iter = board.indexOf(4);
-                                board[iter] = 0;
-                                ComY -= 1;
-                                iter -= gameLevel;
-                                 board[iter] = 4;
-                            } else if (PastMoves[PastMoves.length - 1] === "d") {
-                                let iter = board.indexOf(4);
-                                board[iter] = 0;
-                                ComX -= 1;
-                                iter -= 1;
-                                board[iter] = 4;
-                            } else if (PastMoves[PastMoves.length - 1] === "a") {
-                                let iter = board.indexOf(4);
-                                board[iter] = 0;
-                                ComX += 1;
-                                iter += 1;
-                                board[iter] = 4;
-                            }
-                            PastMoves.pop();
-                        }
-                    }
-                }
-            }
-        } else if (Move === 4) {
-            if (ComX > 0) {
-                if (board[position - 1] != 2) {
-                    PastMoves.push("a");
-                    ComX = MoveLeft(ComX, 1);
-                }
-            }
-            else {
-                for (let x = 0; x < 2; ++x) {
-                    if (PastMoves.length > 3) {
-                        if (PastMoves[PastMoves.length - 1] === 'w') {
-                            let iter = board.indexOf(4);
-                            board[iter] = 0;
-                            ComY += 1;
-                            iter += gameLevel;
-                            board[iter] = 4;
-                        } else if (PastMoves[PastMoves.length - 1] === "s") {
-                            let iter = board.indexOf(4);
-                            board[iter] = 0;
-                            ComY -= 1;
-                            iter -= gameLevel;
-                            board[iter] = 4;
-                        } else if (PastMoves[PastMoves.length - 1] === "d") {
-                            let iter = board.indexOf(4);
-                            board[iter] = 0;
-                            ComX -= 1;
-                            iter -= 1;
-                            board[iter] = 4;
-                        } else if (PastMoves[PastMoves.length - 1] === "a") {
-                            let iter = board.indexOf(4);
-                            board[iter] = 0;
-                            ComX += 1;
-                            iter += 1;
-                            board[iter] = 4;
-                        }
-                        PastMoves.pop();
-                    }
-                }
+            });
+        } else {
+            if (MissedMoves.length === 0) {
+                PosIsOpen = false;
+            } else {
+                let RandomIndex = Math.floor(Math.random() * MissedMoves.length);
+                let RandomPosition = MissedMoves[RandomIndex];
+                MissedMoves.splice(RandomIndex, 1);
+                board[PastPosition] = 0;
+                board[RandomPosition] = 4;
             }
         }
     }
+
+    board[board.indexOf(4)] = 2;
 
     for (let x = 0; x < board.length; ++x) {
         if (board[x] === 2) {
@@ -276,7 +87,6 @@ function CreateMap() {
 
     board[0] = 1;
     board[board.length - 1] = 3;
-    console.log(board);
     DisplayMap();
 }
 
@@ -295,3 +105,117 @@ function DisplayMap() {
 }
 
 CreateMap();
+
+let Player = {
+    x: 0,
+    y: 0,
+    MoveUp(pos) {
+        var iter = board.indexOf(1);
+        board[iter] = 0;
+        
+        if (pos > 0) {
+            pos -= 1;
+            iter += gameLevel * -1;
+        }
+    
+        board[iter] = 1
+        return pos;
+    },
+    
+    MoveRight(pos) {
+        var iter = board.indexOf(1);
+        board[iter] = 0;
+    
+        if (pos < gameLevel - 1) {
+            pos += 1;
+            iter += 1;
+        }
+    
+        board[iter] = 1
+        return pos;
+    },
+    
+    MoveDown(pos) {
+        var iter = board.indexOf(1);
+        board[iter] = 0;
+    
+        if (pos < gameLevel - 1) {
+            pos += 1;
+            iter += gameLevel;
+        }
+    
+        board[iter] = 1
+        return pos;
+    },
+    
+    MoveLeft(pos) {
+        var iter = board.indexOf(1);
+        board[iter] = 0;
+    
+        if (pos > 0)
+        {
+            pos -= 1;
+            iter -= 1;
+        }
+    
+        board[iter] = 1
+        return pos;
+    },
+
+    Died() {
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        context.fillStyle = "rgb(0, 0, 0)";
+        context.font = canvas.width / 20 +  "px Arial";
+        context.fillText("You died. Press r to restart", canvas.width / 4, canvas.height / 2);
+    }
+}
+
+//Counts the certain amounts of values in board
+const countOccurrences = (arr, val) => arr.reduce((a, v) => (v === val ? a + 1 : a), 0);
+
+//Died or not
+let died = false;
+
+window.onkeydown = (e) => {
+    if (!died) {
+        let obstacles = countOccurrences(board, 2);
+        switch (e.key.toLocaleLowerCase(), e.key) {
+            case "w": case "ArrowUp":
+                Player.y = Player.MoveUp(Player.y);
+                break;
+            case "s": case "ArrowDown":
+                Player.y = Player.MoveDown(Player.y);
+                break;
+            case "d": case "ArrowRight":
+                Player.x = Player.MoveRight(Player.x);
+                break;
+            case "a": case "ArrowLeft":
+                Player.x = Player.MoveLeft(Player.x);
+                break;
+            case "r":
+                location.reload();
+                break;
+            default:
+                break;
+        }
+        DisplayMap();
+
+        if (countOccurrences(board, 2) != obstacles) {
+            died = true;
+            Player.Died();
+        }
+
+        if (board[board.length - 1] != 3) {
+            gameLevel += 1;
+            Player.x = 0;
+            Player.y = 0;
+            CreateMap();
+            obstacles = countOccurrences(board, 2);
+        }
+    } else {
+        Player.Died();
+        if (e.key.toLocaleLowerCase() === "r") {
+            location.reload();
+        }
+    }
+}
